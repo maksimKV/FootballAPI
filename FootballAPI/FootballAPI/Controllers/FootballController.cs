401e22afdc821002167d96ebaf5887ea38d6824c
Maksim Kanev <maks_kv@yahoo.com>
2015-12-30 23:31:40 +0200

    Finished the first method - LeagueTable in just under two hours

--- FootballAPI/FootballAPI/Controllers/FootballController.cs
+++ FootballAPI/FootballAPI/Controllers/FootballController.cs
@@ -5,17 +5,94 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using FootballAPI.Models;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace FootballAPI.Controllers
 {
     public class FootballController : ApiController
     { 
+        public string apiURL = "http://api.football-data.org/";
+
         [Route("league/{leagueName}")]
         [HttpGet]
-        public FootballLeague LeagueTable(string leagueName)
+        public IHttpActionResult LeagueTable(string leagueName)
         {
-            FootballLeague league = new FootballLeague { name = leagueName, teams = new string[] { "Test Team 1", "Test Team 2", "Test Team 3" } };
-            return league;
+            using (var seasonClient = new HttpClient())
+            {
+                // I may have to find a way of dynamically changing the season at some point later on
+                seasonClient.BaseAddress = new Uri(apiURL);
+                seasonClient.DefaultRequestHeaders.Accept.Clear();
+                seasonClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage seasonResponse = seasonClient.GetAsync("v1/soccerseasons/?season=2015").Result;
+                if (seasonResponse.IsSuccessStatusCode)
+                {
+                    JArray seasonArray = seasonResponse.Content.ReadAsAsync<JArray>().Result;
+
+                    foreach(JObject seasonObject in seasonArray)
+                    {
+                        string leagueCode = seasonObject["league"].ToString();
+
+                        if (leagueCode == leagueName.ToUpper())
+                        {
+                            string objectURL = seasonObject["_links"]["leagueTable"]["href"].ToString();
+                            string leagueURL = objectURL.Remove(objectURL.IndexOf(apiURL), apiURL.Length);
+
+                            // I need to make a second http call in order to get the required league object
+                            using (var leagueClient = new HttpClient())
+                            {
+                                leagueClient.BaseAddress = new Uri(apiURL);
+                                leagueClient.DefaultRequestHeaders.Accept.Clear();
+                                leagueClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                                HttpResponseMessage leagueResponse = seasonClient.GetAsync(leagueURL).Result;
+                                if (leagueResponse.IsSuccessStatusCode)
+                                {
+                                    JObject leagueObject = leagueResponse.Content.ReadAsAsync<JObject>().Result;
+                                    JArray teamsArray = leagueObject["standing"] as JArray;
+                                    FootballTeam[] leagueTeams = new FootballTeam[teamsArray.Count];
+
+                                    for(int i = 0; i < teamsArray.Count; i++)
+                                    {
+                                        leagueTeams[i] = new FootballTeam
+                                        {
+                                            teamName = teamsArray[i]["teamName"].ToString(),
+                                            leagueRanking = Convert.ToInt32(teamsArray[i]["position"]),
+                                            points = Convert.ToInt32(teamsArray[i]["playedGames"]),
+                                            gamesPlayed = Convert.ToInt32(teamsArray[i]["points"]),
+                                            wins = Convert.ToInt32(teamsArray[i]["wins"]),
+                                            draws = Convert.ToInt32(teamsArray[i]["draws"]),
+                                            losses = Convert.ToInt32(teamsArray[i]["losses"])
+                                        };
+                                    }
+
+                                    FootballLeague league = new FootballLeague
+                                    {
+                                        leagueName = leagueObject["leagueCaption"].ToString(),
+                                        matchday = Convert.ToInt32(leagueObject["matchday"]),
+                                        teams = leagueTeams
+                                    };
+
+                                    return Ok(league);
+                                }
+                                else
+                                {
+                                    return NotFound();
+                                }
+                            }
+                        }
+                    }
+
+                    // Not found compared with user input. Probably user input is wrong
+                    return NotFound();
+                }
+                else
+                {
+                    // Didn't get any result from their API
+                    return NotFound();
+                }
+            }
         }
 
         [Route("team/{teamName}")]
--- FootballAPI/FootballAPI/Models/FootballModels.cs
+++ FootballAPI/FootballAPI/Models/FootballModels.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 namespace FootballAPI.Models
 {
     // Class declarations of objects used in the Football Controller
-
     public class FootballLeague
     {
-        public string name { get; set; }
-        public string[] teams { get; set; }
+        public string leagueName { get; set; }
+        public int matchday { get; set; }
+        public FootballTeam[] teams { get; set; }
     }
 
     public class FootballTeam
     {
-        public string name { get; set; }
+        public string teamName { get; set; }
+        public int leagueRanking { get; set; }
         public int points { get; set; }
+        public int gamesPlayed { get; set; }
+        public int wins { get; set; }
+        public int draws { get; set; }
+        public int losses { get; set; }
     }
 }
